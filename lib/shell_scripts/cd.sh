d12fe566717a54edb907156eb630013d3e99a248
jawerty <jawerty210@gmail.com>
2013-03-28 18:08:53 -0400

    first commit

--- lib/shell_scripts/cd.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cd mywebsite && sh ./heroku_start.sh
--- test.js
+++
@@ -1,7 +0,0 @@
-exec = require('child_process').exec;
-
-exec('sh ./lib/shell_scripts/heroku_start.sh', function (error, stdout, stderr){
-	console.log('err: '+error)
-	console.log('out: '+stdout);
-	console.log('err: '+stderr)
-})
